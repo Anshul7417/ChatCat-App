eba8be5693d3f0e2fdc8af53d5cf15aa053b829c
Anshul Tripathi <tripathianshul06@gmail.com>
2022-04-22 00:27:01 +0530

    comments added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const socket = io('http://localhost:8000');
 
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp')
-const messageContainer = document.querySelector(".container")
+const form = document.getElementById('send-container');  // it acquires full form
+const messageInput = document.getElementById('messageInp')   // input field
+const messageContainer = document.querySelector(".container")     // container for dynamic messages
 
 var audio = new Audio('notification.mp3')
 
-const append = (message, position) => {
+const append = (message, position) => {    // this function is creating dynamic divs for all operations
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
@@ -18,19 +18,19 @@ const append = (message, position) => {
 }
 
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener('submit', (e) => {   // it targets submit button
+    e.preventDefault();    // it prevents the page from going to default state
     const message = messageInput.value;
-    append(`You: ${message}`, 'right');
+    append(`You: ${message}`, 'right'); 
     socket.emit('send', message);
     messageInput.value = '';
 })
 
 
 
-const username = prompt("Enter your name to join to Chat-Cat");
+const username = prompt("Enter your name to join to Chat-Cat","Guest");
 
-socket.emit('new-user-joined', username);
+socket.emit('new-user-joined', username); 
 
 socket.on('user-joined', username => {
     append(`${username} joined the chat`, 'left')
