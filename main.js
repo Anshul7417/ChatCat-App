212a3986e0aaa65f21300a38fdfbb1ba28e82a90
Anshul Tripathi <tripathianshul06@gmail.com>
2022-11-05 12:15:33 +0530

    Done

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-const socket = io('http://localhost:8000');
-
 const form = document.getElementById('send-container');  // it acquires full form
-const messageInput = document.getElementById('messageInp')   // input field
-const messageContainer = document.querySelector(".container")     // container for dynamic messages
+const messageInput = document.getElementById('messageInp');   // input field
+const messageContainer = document.querySelector('.container');
+var socket = io();    //starting socket   
 
 var audio = new Audio('notification.mp3')
 
@@ -17,6 +16,18 @@ const append = (message, position) => {    // this function is creating dynamic 
     }
 }
 
+function onUserNameSubmit() {
+    var userDiv = document.getElementsByClassName('userdiv')[0];
+    var user = document.getElementsByClassName('username')[0].value;
+    var chatDiv = document.getElementsByClassName('chat')[0];
+    if(user.length > 0) {
+        userDiv.classList.add('hide');   //class hiding
+        chatDiv.classList.remove('hide');
+        socket.emit('new-user-joined', user);
+    } else {
+        alert('Username Field cannot be null');
+    }
+}
 
 form.addEventListener('submit', (e) => {   // it targets submit button
     e.preventDefault();    // it prevents the page from going to default state
@@ -26,22 +37,14 @@ form.addEventListener('submit', (e) => {   // it targets submit button
     messageInput.value = '';
 })
 
-
-
-const username = prompt("Enter your name to join to Chat-Cat","Guest");
-
-socket.emit('new-user-joined', username); 
-
-socket.on('user-joined', username => {
-    append(`${username} joined the chat`, 'left')
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'left')
 })
 
 socket.on('receive', data => {
     append(`${data.name}: ${data.message}`, 'left')
 });
 
-
 socket.on('left', name => {
     append(`${ name } left the chat`, 'left')
 })
-
